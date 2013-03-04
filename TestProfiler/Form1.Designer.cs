88d58fb08acc7143d188708a15596f9bf10ca159
Ricky Yau <wingkinyau@gmail.com>
2013-03-03 23:06:08 -0800

    test

---
+++ TestProfiler/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TestProfiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest1 = new System.Windows.Forms.Button();
+            this.btnTest2 = new System.Windows.Forms.Button();
+            this.btnTest3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.Location = new System.Drawing.Point(192, 39);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(75, 23);
+            this.btnTest1.TabIndex = 0;
+            this.btnTest1.Text = "Test 1";
+            this.btnTest1.UseVisualStyleBackColor = true;
+            this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
+            // 
+            // btnTest2
+            // 
+            this.btnTest2.Location = new System.Drawing.Point(192, 99);
+            this.btnTest2.Name = "btnTest2";
+            this.btnTest2.Size = new System.Drawing.Size(75, 23);
+            this.btnTest2.TabIndex = 0;
+            this.btnTest2.Text = "Test 2";
+            this.btnTest2.UseVisualStyleBackColor = true;
+            this.btnTest2.Click += new System.EventHandler(this.btnTest2_Click);
+            // 
+            // btnTest3
+            // 
+            this.btnTest3.Location = new System.Drawing.Point(192, 160);
+            this.btnTest3.Name = "btnTest3";
+            this.btnTest3.Size = new System.Drawing.Size(75, 23);
+            this.btnTest3.TabIndex = 0;
+            this.btnTest3.Text = "Test 3";
+            this.btnTest3.UseVisualStyleBackColor = true;
+            this.btnTest3.Click += new System.EventHandler(this.btnTest3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 255);
+            this.Controls.Add(this.btnTest3);
+            this.Controls.Add(this.btnTest2);
+            this.Controls.Add(this.btnTest1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest1;
+        private System.Windows.Forms.Button btnTest2;
+        private System.Windows.Forms.Button btnTest3;
+    }
+}
+
---
+++ TestProfiler/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TestProfiler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest1_Click(object sender, EventArgs e)
+        {
+            HoldingTask(1000);
+            HoldingTask(2000);
+            HoldingTask(5000);
+        }
+
+        
+
+        private void btnTest2_Click(object sender, EventArgs e)
+        {
+            GetSomeResponseHtml("http://www.google.com");
+            GetSomeResponseHtml("http://www.yahoo.com");
+            GetSomeResponseHtml("http://www.nodus.com");
+        }
+
+        private void btnTest3_Click(object sender, EventArgs e)
+        {
+            var datatableResult = SomeDatabaseAction();
+            var line = string.Empty;
+            for (int i = 0; i < datatableResult.Rows.Count; i++)
+            {
+                var row = datatableResult.Rows[i];
+                StringBuilder sb = new StringBuilder();
+                sb.Append(row[0]);
+                for (int j = 1; j < row.ItemArray.Length; j++)
+                {
+                    sb.Append("," + row[j]);
+                }
+                sb.Append("\r\n");
+                line += sb.ToString();
+            }
+            LogToFile(line);
+        }
+
+        private void HoldingTask(int millisecond)
+        {
+            System.Threading.Thread.Sleep(millisecond);
+        }
+
+        private string GetSomeResponseHtml(string url)
+        {
+            var htmlSource = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                htmlSource = sr.ReadToEnd();
+            }
+            return htmlSource;
+        }
+
+        private DataTable SomeDatabaseAction()
+        {
+            string connectionString = "server=chocolate;database=SchoolEnrollment;uid=sa;password=goldfish";            
+            DataTable result = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = conn;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "GetAllApplicationConfiguration";
+                    using (SqlDataReader dr = command.ExecuteReader())
+                    {
+                        result.Load(dr);
+                    }
+                }
+                conn.Close();
+            }
+            return result;
+        }
+
+        private void LogToFile(string message)
+        {
+            var filename = @"c:\temp\test.log";
+            using (StreamWriter sw = new StreamWriter(filename, true))
+            {
+                lock (sw)
+                {
+                    try
+                    {
+                        string timeStamp = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+                        string logMessage = timeStamp + ":\t" + message;
+                        sw.WriteLine(logMessage);
+                        sw.Flush();
+                    }
+                    finally
+                    {
+                        sw.Close();
+                    }
+                }
+            }
+        }
+
+    }
+}
